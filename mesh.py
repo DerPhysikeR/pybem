179d7bcf1c0926ce22bc173f573ec3150322c11d
Paul Reiter <reiter.paul@gmail.com>
2019-05-05 10:13:19 +0200

    simplify Mesh class and make it stateless

--- mesh.py
+++ mesh.py
@@ -9,40 +9,41 @@ import numpy as np
 class Mesh:
 
     def __init__(self, nodes, elements):
-        self.nodes = np.array(nodes)
-        self.elements = np.array(elements)
-        self._centers = None
-        self._normals = None
+        self._nodes = np.array(nodes)
+        assert self._nodes.shape[1] == 2
+        self._elements = np.array(elements)
+        assert self._elements.shape[1] == 2
+        self._centers = self._calc_centers()
+        self._normals = self._calc_normals()
+
+    @property
+    def nodes(self):
+        return self._nodes
+
+    @property
+    def elements(self):
+        return self._elements
 
     @property
     def centers(self):
-        if self._centers is None:
-            self._centers = np.empty((self.elements.shape[0],
-                                      self.nodes.shape[1]))
-            for i, corners in enumerate(self.elements):
-                self._centers[i] = np.vstack([self.nodes[j]
-                                              for j in corners]).mean(axis=0)
         return self._centers
 
     @property
     def normals(self):
-        if self._normals is None:
-            if self.nodes.shape[1] == 2:
-                self._normals = self.calc_2d_normals()
-            elif self.nodes.shape[1] == 3:
-                self._normals = self.calc_3d_normals()
-            else:
-                raise ValueError('Mesh is neither 2D nor 3D!')
         return self._normals
 
-    def calc_2d_normals(self):
-        normals = np.empty((self.elements.shape[0], self.nodes.shape[1]))
-        for i, corners in enumerate(self.elements):
-            element_vector = self.nodes[corners[1]] - self.nodes[corners[0]]
+    def _calc_centers(self):
+        centers = np.empty((self._elements.shape[0], self._nodes.shape[1]))
+        for i, corners in enumerate(self._elements):
+            centers[i] = np.vstack([self._nodes[j]
+                                    for j in corners]).mean(axis=0)
+        return centers
+
+    def _calc_normals(self):
+        normals = np.empty((self._elements.shape[0], self._nodes.shape[1]))
+        for i, corners in enumerate(self._elements):
+            element_vector = self._nodes[corners[1]] - self._nodes[corners[0]]
             element_length = np.sqrt(element_vector.dot(element_vector))
             normals[i] = element_vector.dot(
                 np.array([[0, -1], [1, 0]]))/element_length
         return normals
-
-    def calc_3d_normals(self):
-        raise NotImplementedError('3D normals are not supported yet!')
--- test_mesh.py
+++ test_mesh.py
@@ -18,5 +18,5 @@ def test_mesh_centers(nodes, elements, center):
 @pytest.mark.parametrize('nodes, elements, normals', [
     ([[0, 0], [2, 0], [2, 2]], [[0, 1], [1, 2]], np.array([[0, -1], [1, 0]])),
 ])
-def test_mesh_2d_normals(nodes, elements, normals):
+def test_mesh_normals(nodes, elements, normals):
     np.testing.assert_allclose(normals, Mesh(nodes, elements).normals)
