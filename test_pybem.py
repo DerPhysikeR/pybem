bb2922b092a401bd35f1d85beb8b5bf178b6fd48
Paul Reiter <paul.reiter@ait.ac.at>
2019-05-08 10:31:39 +0200

    add test for fully reflective mesh

--- test_pybem.py
+++ test_pybem.py
@@ -4,6 +4,7 @@
 @author: Paul Reiter
 """
 import numpy as np
+import pytest
 from mesh import Mesh
 from pybem import complex_system_matrix, calc_scattered_pressure_at
 from integrals import g_2d, admitant_2d_integral
@@ -35,3 +36,24 @@ def test_calc_scattered_pressure_at():
     np.testing.assert_allclose(g_2d(k, np.array([0., .5]),
                                     np.array([0., -1.])),
                                solution[0], rtol=1e-3)
+
+
+@pytest.mark.slow
+def test_calc_scattered_pressure_at_point_source_reflective_plane():
+    # actually solve the linear system for point source above reflective plane
+    n = 80
+    # create admitant mesh
+    mesh = Mesh([(x, 0) for x in np.linspace(4, -4, n+1)],
+                [(i, i+1) for i in range(n)])
+    k, rho, omega = 2*np.pi*300/343, 1, 2*np.pi*300
+    system_matrix = complex_system_matrix(mesh, admitant_2d_integral, k, rho,
+                                          omega)
+    p_incoming = np.array([g_2d(k, point, np.array([0., 1.]))
+                           for point in mesh.centers], dtype=complex)
+    surface_pressure = np.linalg.solve(system_matrix, -p_incoming)
+    solution = calc_scattered_pressure_at(mesh, admitant_2d_integral, k,
+                                          surface_pressure,
+                                          np.array([[0., .5]]), rho, omega)
+    np.testing.assert_allclose(g_2d(k, np.array([0., .5]),
+                                    np.array([0., -1.])),
+                               solution[0], rtol=1e-2)
