fix signs of BEM formulation after new consistent derivation

pybem/integrals.py
@@ -37,14 +37,14 @@ def complex_quad(function, *args, **kwargs):
 
 def admitant_2d_integral(k, r, admittance, n, corners, singular, rho, c):
     def integral_function(rs):
-        return h_2d(n, k, r, rs) + 1j*k*c*rho*admittance*g_2d(k, r, rs)
+        return h_2d(n, k, r, rs) - 1j*k*c*rho*admittance*g_2d(k, r, rs)
     return (line_integral(integral_function, corners[0], corners[1], singular)
-            - singular/2)
+            + singular/2)
 
 
 def h_2d(n, k, r, rs):
     distance = np.sqrt((r-rs).dot(r-rs))
-    scaling = 1j*k*n.dot(r - rs)/distance/8
+    scaling = 1j*k*n.dot(rs - r)/distance/8
     return scaling*(hankel2(-1, k*distance) - hankel2(1, k*distance))
 
 

pybem/pybem.py
@@ -25,8 +25,8 @@ def calc_scattered_pressure_at(mesh, integral_function, k, surface_pressure,
     solution = np.zeros(len(microphone_points), dtype=complex)
     for i, point in enumerate(microphone_points):
         for j, sp in enumerate(surface_pressure):
-            solution[i] += sp*integral_function(k, point, mesh.admittances[j],
-                                                mesh.normals[j],
-                                                mesh.corners[j], False,
-                                                *args, **kwargs)
+            solution[i] += -sp*integral_function(k, point, mesh.admittances[j],
+                                                 mesh.normals[j],
+                                                 mesh.corners[j], False,
+                                                 *args, **kwargs)
     return solution

tests/cylinder_scattering_integration_test.py
@@ -15,8 +15,8 @@ def calc_coefficiencts(k, radius, rho_c, amplitude, admittance, max_order):
     orders = np.arange(max_order+1)
     thetas = 2*np.pi*orders/len(orders)
     rhs = (amplitude*np.exp(1j*k*radius*np.cos(thetas)) *
-           (admittance + np.cos(thetas)/rho_c))
-    matrix = np.array([[np.cos(n*theta) * (1j*h2vp(n, k*radius)/rho_c -
+           (np.cos(thetas)/rho_c - admittance))
+    matrix = np.array([[np.cos(n*theta) * (1j*h2vp(n, k*radius)/rho_c +
                                            admittance*hankel2(n, k*radius))
                         for n in orders]
                        for theta in thetas])
@@ -39,7 +39,7 @@ def complex_relative_error(reference, to_test):
 
 
 @pytest.mark.parametrize('ka', [.5, 2])
-@pytest.mark.parametrize('admittance', [0, 343])
+@pytest.mark.parametrize('admittance', [0, 1/343])
 @pytest.mark.slow
 def test_plane_wave_admittance_cylinder_scattering(ka, admittance):
     # set constants
@@ -71,7 +71,7 @@ def test_plane_wave_admittance_cylinder_scattering(ka, admittance):
     p_incoming = np.array([amplitude*np.exp(1j*k*point[0])
                            for point in mesh.centers])
     matrix = pb.complex_system_matrix(mesh, pb.admitant_2d_integral, k, rho, c)
-    surface_pressure = np.linalg.solve(matrix, -p_incoming)
+    surface_pressure = np.linalg.solve(matrix, p_incoming)
     result = pb.calc_scattered_pressure_at(mesh, pb.admitant_2d_integral, k,
                                            surface_pressure, mic_points, rho,
                                            c)

tests/test_integrals.py
@@ -26,7 +26,7 @@ def test_g_2d(k, r, rs, solution):
      1j/8 * (hankel2(-1, 2) - hankel2(1, 2))),
 ])
 def test_h_2d(n, k, r, rs, solution):
-    assert solution == -pb.h_2d(n, k, r, rs)
+    assert solution == pb.h_2d(n, k, r, rs)
 
 
 def test_line_integral():
@@ -70,4 +70,4 @@ def test_admitant_2d_integral_fully_reflective_single_plane():
 def test_admitant_2d_integral_fully_reflective_single_plane_singular():
     result = pb.admitant_2d_integral(1, np.array([0, 0]), 0, np.array([1, 0]),
                                      np.array([[0, -.5], [0, .5]]), True, 1, 1)
-    np.testing.assert_almost_equal(-1/2, result)
+    np.testing.assert_almost_equal(1/2, result)

tests/test_pybem.py
@@ -53,7 +53,7 @@ def test_calc_scattered_pressure_at_point_source_reflective_plane():
                                              rho, c)
     p_incoming = np.array([pb.g_2d(k, point, np.array([0., 1.]))
                            for point in mesh.centers], dtype=complex)
-    surface_pressure = np.linalg.solve(system_matrix, -p_incoming)
+    surface_pressure = np.linalg.solve(system_matrix, p_incoming)
     solution = pb.calc_scattered_pressure_at(mesh, pb.admitant_2d_integral, k,
                                              surface_pressure,
                                              np.array([[0., .5]]), rho, c)
@@ -73,7 +73,7 @@ def test_calc_scattered_pressure_at_normal_plane_wave_admittance_plane():
     system_matrix = pb.complex_system_matrix(mesh, pb.admitant_2d_integral, k,
                                              rho, c)
     p_incoming = np.array([1. + 1j for point in mesh.centers], dtype=complex)
-    surface_pressure = np.linalg.solve(system_matrix, -p_incoming)
+    surface_pressure = np.linalg.solve(system_matrix, p_incoming)
     solution = pb.calc_scattered_pressure_at(mesh, pb.admitant_2d_integral, k,
                                              surface_pressure,
                                              np.array([[0., .5]]), rho, c)