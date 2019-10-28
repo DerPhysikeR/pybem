2ad6e7c66d63621d30016e3cfb5285c92f8db625
Paul Reiter <paul.reiter@ait.ac.at>
2019-10-28 11:30:40 +0100

    Improve `black` auto-formatting of equations with parenthesis

--- pybem/helmholtz/burton_miller.py
+++ pybem/helmholtz/burton_miller.py
@@ -27,12 +27,7 @@ def admitant_2d_matrix_element_bm(k, mesh, row_idx, col_idx, rho, c, coupling_si
         length = np.sqrt(element_vector.dot(element_vector))
         lk2 = length * k / 2
         return (
-            +z0
-            * admittance
-            * np.pi
-            * length
-            * k
-            / 8
+            +(z0 * admittance * np.pi * length * k / 8)
             * (+hankel2(0, lk2) * struve(-1, lk2) + hankel2(1, lk2) * struve(0, lk2))
             - coupling_sign
             * fixed_quad(regularized_hypersingular_bm_part, 0, lk2)[0]
@@ -61,14 +56,9 @@ def hypersingular(k, r, rs, n, ns):
     vector = r - rs
     distance = np.sqrt(vector.dot(vector))
     kdist = k * distance
-    return (
-        1j
-        * k
-        / (4 * distance ** 2)
-        * (
-            distance * hankel2(1, kdist) * n.dot(ns)
-            - k * hankel2(2, kdist) * n.dot(vector) * ns.dot(vector)
-        )
+    return (1j * k / (4 * distance ** 2)) * (
+        distance * hankel2(1, kdist) * n.dot(ns)
+        - k * hankel2(2, kdist) * n.dot(vector) * ns.dot(vector)
     )
 
 
