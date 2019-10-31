58ab0311ea7c1c3e9fd6f7308ad87c95ab4b839a
Paul Reiter <paul.reiter@ait.ac.at>
2019-10-31 10:44:49 +0100

    Refactor fast_burton_miller to allow for acceleration

--- pybem/helmholtz/fast_burton_miller.pyx
+++ pybem/helmholtz/fast_burton_miller.pyx
@@ -8,12 +8,20 @@ from scipy.integrate import fixed_quad
 from scipy.special import hankel2, struve
 
 
-def complex_system_matrix(matrix_element_function, mesh, *args, **kwargs):
+def complex_system_matrix(mesh, *args, **kwargs):
     dimension = len(mesh.elements)
     system_matrix = np.empty((dimension, dimension), dtype=complex)
-    for i, row in enumerate(system_matrix):
-        for j, col in enumerate(row):
-            system_matrix[i, j] = matrix_element_function(mesh, i, j, *args, **kwargs)
+    for i, diag in enumerate(system_matrix):
+        system_matrix[i, i] = main_diagonal(mesh, i, *args, **kwargs)
+    rest_of_the_matrix(
+        system_matrix,
+        mesh.normals,
+        mesh.centers,
+        mesh.corners,
+        mesh.admittances,
+        *args,
+        **kwargs
+    )
     return system_matrix
 
 
@@ -31,37 +39,54 @@ def regularized_hypersingular_bm_part(v):
     return hankel2(1, v) / v - 2j / (np.pi * v ** 2)  # regularization
 
 
-def admitant_2d_matrix_element_bm(mesh, row_idx, col_idx, z0, k, coupling_sign):
-    n, ns = mesh.normals[row_idx], mesh.normals[col_idx]
-    r = mesh.centers[row_idx]
-    corners, admittance = mesh.corners[col_idx], mesh.admittances[col_idx]
-    singular = row_idx == col_idx
+def main_diagonal(mesh, idx, z0, k, coupling_sign):
+    corners, admittance = mesh.corners[idx], mesh.admittances[idx]
+    element_vector = corners[1] - corners[0]
+    length = np.sqrt(element_vector.dot(element_vector))
+    lk2 = length * k / 2
+    return (
+        +(z0 * admittance * np.pi * length * k / 8)
+        * (+hankel2(0, lk2) * struve(-1, lk2) + hankel2(1, lk2) * struve(0, lk2))
+        - coupling_sign * fixed_quad(regularized_hypersingular_bm_part, 0, lk2)[0] / 2
+        + coupling_sign * 2j / (np.pi * k * length)
+        + (1 - coupling_sign * z0 * admittance) / 2
+    )
 
-    if singular:
-        element_vector = corners[1] - corners[0]
-        length = np.sqrt(element_vector.dot(element_vector))
-        lk2 = length * k / 2
-        return (
-            +(z0 * admittance * np.pi * length * k / 8)
-            * (+hankel2(0, lk2) * struve(-1, lk2) + hankel2(1, lk2) * struve(0, lk2))
-            - coupling_sign
-            * fixed_quad(regularized_hypersingular_bm_part, 0, lk2)[0]
-            / 2
-            + coupling_sign * 2j / (np.pi * k * length)
-            + (1 - coupling_sign * z0 * admittance) / 2
-        )
 
-    else:
+def rest_of_the_matrix(
+    matrix,
+    mesh_normals,
+    mesh_centers,
+    mesh_corners,
+    mesh_admittances,
+    z0,
+    k,
+    coupling_sign,
+):
+    for row_idx, row in enumerate(matrix):
+        for col_idx, col in enumerate(row):
+            if row_idx == col_idx:
+                continue
 
-        def integral_function(rs):
-            return (
-                hs_2d(ns, k, r, rs)
-                - 1j * k * z0 * admittance * g_2d(k, r, rs)
-                + coupling_sign * z0 * admittance * h_2d(n, k, r, rs)
-                + coupling_sign * 1j / k * hypersingular(k, r, rs, n, ns)
+            n, ns = mesh_normals[row_idx], mesh_normals[col_idx]
+            r = mesh_centers[row_idx]
+            corners, admittance = mesh_corners[col_idx], mesh_admittances[col_idx]
+
+            element_vector = corners[1] - corners[0]
+            length = np.sqrt(element_vector.dot(element_vector))
+            lk2 = length * k / 2
+
+            def integral_function(rs):
+                return (
+                    hs_2d(ns, k, r, rs)
+                    - 1j * k * z0 * admittance * g_2d(k, r, rs)
+                    + coupling_sign * z0 * admittance * h_2d(n, k, r, rs)
+                    + coupling_sign * 1j / k * hypersingular(k, r, rs, n, ns)
+                )
+
+            matrix[row_idx, col_idx] = line_integral(
+                integral_function, corners[0], corners[1]
             )
-
-        return line_integral(integral_function, corners[0], corners[1])
 
 
 def hypersingular(k, r, rs, n, ns):
@@ -100,9 +125,9 @@ def g_2d(k, r, rs):
     return 1j * hankel2(0, k * np.sqrt((r - rs).dot(r - rs))) / 4
 
 
-def fast_burton_miller_solver(mesh, p_incoming, grad_p_incoming, z0, k, coupling_sign=-1):
-    matrix = complex_system_matrix(
-        admitant_2d_matrix_element_bm, mesh, z0, k, coupling_sign
-    )
+def fast_burton_miller_solver(
+    mesh, p_incoming, grad_p_incoming, z0, k, coupling_sign=-1
+):
+    matrix = complex_system_matrix(mesh, z0, k, coupling_sign)
     rhs = burton_miller_rhs(mesh, p_incoming, grad_p_incoming, k, coupling_sign)
     return np.linalg.solve(matrix, rhs)
